de519a11f47bac1a47699995bc76c2f0b6f4b651
arian921017 <arianwithcode@gmail.com>
2025-03-21 17:17:00 +0800

    feat: add public varible

---
+++ contracts/Hotel.sol
@@ -0,0 +1,11 @@
+ // SPDX-License-Identifier: MIT
+
+ pragma solidity ^0.8.23;
+
+ // First compile and then deploy
+ contract MyFirstContract {
+    string  public hey = "Hello Bob";
+    uint256 public no = 45;
+ }
+ 
+ 
