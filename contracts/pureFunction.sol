0de6ddbb82dd6a9e965c8b530390fb26fac72db9
arian921017 <arianwithcode@gmail.com>
2025-03-26 13:44:12 +0800

    feat: fix errors

--- contracts/pureFunction.sol
+++ contracts/pureFunction.sol
@@ -19,16 +19,16 @@ contract PureFunction {
         return (noNum1, noNum2);
       }
 
-       function getData2() public pure returns (uint256, uint256) {
-        uint256 noNum1 = 344;
-        uint256 noNum2 = 225;
+      //  function getData2() public pure returns (uint256, uint256) {
+      //   uint256 noNum1 = 344;
+      //   uint256 noNum2 = 225;
 
-        // TypeError: Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
-        uint256 product = num1 + noNum1;
-        uint256 total = num2 + noNum2;
+      //   // TypeError: Function declared as pure, but this expression (potentially) reads from the environment or state and thus requires "view".
+      //   uint256 product = num1 + noNum1;
+      //   uint256 total = num2 + noNum2;
 
-        return (product, total);
-      }
+      //   return (product, total);
+      // }
 
       function getData3() public view  returns (uint256, uint256) {
         uint256 noNum1 = 344;
