665e8525305fcdeaf11520da67f158ba4f84593f
arian921017 <arianwithcode@gmail.com>
2025-03-27 15:27:53 +0800

    feat: add enum

---
+++ contracts/enum.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+contract Enum {
+   enum Status {
+        Pending,
+        Shipped,
+        Accepted,
+        Rejected,
+        Canceled
+    }
+    // Returns uint
+    // Pending - 0
+    // Shipped - 1
+    // Accepted - 2
+    // Rejected - 3
+    // Canceled - 4
+
+    Status public status;
+
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    function set(Status _status) public  {
+        status = _status;
+    }
+
+    function cancel() public  {
+        status = Status.Canceled;
+    }
+
+    function reset() public  {
+        delete status;
+    }
+}
