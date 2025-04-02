65bc795bd250a67a4a5a88fab8a4021cae78efa6
arianshi <arianwithcode@gmail.com>
2025-04-02 17:21:49 +0800

    feat: add reentrancy

---
+++ contracts/reentrancyAttackInSmartContract.sol
@@ -0,0 +1,12 @@
+// Definition of a Reentrancy Attack
+// Unsafe external call(s) that allow(s) malicious manipulation of the internal and/or associated external contract state(s).
+
+/*
+    Types of Reentrancy Attacks
+    Single-Function Reentrancy
+    Cross-Function Reentrancy
+    Cross-Contract Reentrancy
+    Cross-Chain Reentrancy
+    Read-Only Reentrancy
+
+*/
