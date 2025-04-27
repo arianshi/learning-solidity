ad4f70942afbac04f9a94f9228d24a53c92eda8a
arianshi <arianwithcode@gmail.com>
2025-04-27 13:52:16 +1000

    feat: add solidity introduction

---
+++ contracts/solidityIntroduction.sol
@@ -0,0 +1,17 @@
+/*
+Rust: fast but no community no libraries support
+Vyper: some libraries
+GO: 
+Yul: 
+Solidity: static type langs and one contract includes muti-contact
+*/
+
+/*
+ Solidity:
+  keywords
+    1. public, private, internal, external, payable, view
+    2. modifier function(){}
+  Identifiers
+  Literals
+  Punctuation
+*/
